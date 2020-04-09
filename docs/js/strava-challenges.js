0d77a568315348b23b07bf35a666eae6967d0223
pl31 <3818398+pl31@users.noreply.github.com>
2020-04-09 22:57:39 +0200

    Update strava-challenges.js

--- docs/js/strava-challenges.js
+++ docs/js/strava-challenges.js
@@ -13,10 +13,13 @@ stravaChallenges.updateUI = function() {
 stravaChallenges.iframeReady = function(index, element) {
     // increment loaded segments
     stravaChallenges.data.segments_loaded++;
+    
+    if (stravaChallenges.data.segments_loaded == stravaChallenges.data.segments_count) {
     // get leaderboard tables
-    var leaderboard_tables = $(element).find("div.leaderboards table");
-    var leaderboard_table_overall = $(leaderboard_tables).eq(1)
-    var leaderboard_table_women = $(leaderboard_tables).eq(2)
+        var leaderboard_tables = $(element).find("div.leaderboards table");
+        var leaderboard_table_overall = $(leaderboard_tables).eq(1)
+        var leaderboard_table_women = $(leaderboard_tables).eq(2)
+    }
 }
 
 // start point ("ready" method)
